0cfe9c88a7e37bca8bbb82fe03c7e07a106febbb
Paul <paul@debian.paul>
2021-05-13 17:43:39 +0200

    add corrections in select mode

--- srcs/selection_management.c
+++ srcs/selection_management.c
@@ -6,12 +6,12 @@
 /*   By: pbrochar <pbrochar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/12 14:45:01 by pbrochar          #+#    #+#             */
-/*   Updated: 2021/05/13 17:28:00 by pbrochar         ###   ########.fr       */
+/*   Updated: 2021/05/13 17:43:04 by pbrochar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
+#include "colors.h"
 // a = select all
 // c = copy
 // x = cut
@@ -138,6 +138,23 @@ int		key_is_term_select(t_master *msh, char *buf)
 	return (-1);
 }
 
+void	unselect_for_leave(t_master *msh)
+{
+	if (msh->select->begin->curs_pos_abs == -1 ||
+		msh->select->begin->curs_pos_abs == -1)
+		return ;
+	if (msh->select->begin->curs_pos_abs > msh->select->end->curs_pos_abs)
+		swap_select_curs(msh);
+	save_curs_pos(msh);
+	mv_curs_abs(msh, msh->select->begin->curs_pos_abs % msh->res_x,\
+		msh->select->end->curs_pos_abs / msh->res_x);
+	write(1, &msh->line[msh->select->begin->curs_pos_rel],\
+		msh->select->end->curs_pos_abs - msh->select->begin->curs_pos_abs);
+	mv_curs_abs(msh, msh->save_curs_pos->curs_pos_abs % msh->res_x, \
+			msh->save_curs_pos->curs_pos_abs / msh->res_x);
+	rest_curs_pos(msh);
+}
+
 void	loop_selection(t_master *msh)
 {
 	int ret;
@@ -157,25 +174,35 @@ void	loop_selection(t_master *msh)
 		}
 		else
 		{
-			msh->select->is_select = 0;
+			unselect_for_leave(msh);
 			return ;
 		}
 		ft_bzero(buf, 50);
 	}
 }
 
+void	print_mode(t_master *msh, char c, char *color)
+{
+	save_curs_pos(msh);
+	mv_curs_abs(msh, msh->prompt_len - 3, 0);
+	write(1, color, ft_strlen(color));
+	write(1, &c, 1);
+	write(1, TEXT_NORMAL, 4);
+	mv_curs_abs(msh, msh->save_curs_pos->curs_pos_abs % msh->res_x,\
+				msh->save_curs_pos->curs_pos_abs / msh->res_x);
+	rest_curs_pos(msh);
+}
+
 void	select_mode(t_master *msh)
 {
 	tputs(msh->term->inv_curs, 1, ft_putchar);
 	msh->select->is_select = 1;
-	/*save_curs_pos(msh);
-	mv_curs_abs(msh, msh->prompt_len - 3, 0);
-	write(1, "i", 1);
-	rest_curs_pos(msh);
-	mv_curs_abs(msh, msh->curs_pos->curs_pos_abs % msh->res_x,\
-				msh->curs_pos->curs_pos_abs / msh->res_x);*/
+	print_mode(msh, 'i', TEXT_RED);
 	loop_selection(msh);
+	set_alt_curs_pos(msh, msh->select->begin, -1);
+	set_alt_curs_pos(msh, msh->select->end, -1);
 	msh->select->is_select = 0;
+	print_mode(msh, 'n', TEXT_NORMAL);
 	tputs(msh->term->vis_curs, 1, ft_putchar);
 }
 
