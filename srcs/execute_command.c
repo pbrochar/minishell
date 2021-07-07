5fe2fe939fb45c6ab13150c6b15896191c3f4d76
Paul <paul@debian.paul>
2021-07-07 13:29:46 +0200

    add corrections

--- srcs/execute_command.c
+++ srcs/execute_command.c
@@ -6,7 +6,7 @@
 /*   By: pbrochar <pbrochar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/05 10:58:21 by pbrochar          #+#    #+#             */
-/*   Updated: 2021/07/07 13:12:44 by pbrochar         ###   ########.fr       */
+/*   Updated: 2021/07/07 13:29:21 by pbrochar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ char	*add_path_in_command(t_master *msh, char *name, int path_index)
 {
 	char	*command;
 	size_t	command_len;
-	
+
 	command_len = ft_strlen(msh->path[path_index]) + ft_strlen(name) + 2;
 	command = malloc(sizeof(char) * command_len);
 	if (command == NULL)
@@ -27,11 +27,33 @@ char	*add_path_in_command(t_master *msh, char *name, int path_index)
 	return (command);
 }
 
+static int	manage_command(t_master *msh, char **arg, char **command)
+{
+	int	i;
+
+	i = search_command(msh, arg);
+	if (i == -1)
+	{
+		ft_printf("msh: command not found: %s\n", arg[0]);
+		return (-1);
+	}
+	*command = add_path_in_command(msh, arg[0], i);
+	return (0);
+}
+
+static void	parent_wait_pid(t_master *msh)
+{
+	int	return_value;
+
+	waitpid(msh->pid, &return_value, 0);
+	if (WIFEXITED(return_value))
+		msh->return_value = WEXITSTATUS(return_value);
+}
+
 int	exec_command(t_master *msh, char **arg)
 {
-	int		i;
+	int		ret;
 	char	*command;
-	int		return_value;
 
 	if (arg[0] == NULL || arg[0][0] == 32)
 		return (-1);
@@ -39,13 +61,9 @@ int	exec_command(t_master *msh, char **arg)
 		command = ft_strdup(arg[0]);
 	else
 	{
-		i = search_command(msh, arg);
-		if (i == -1)
-		{
-			ft_printf("msh: command not found: %s\n", arg[0]);
+		ret = manage_command(msh, arg, &command);
+		if (ret == -1)
 			return (-1);
-		}
-		command = add_path_in_command(msh, arg[0], i);
 	}
 	tcsetattr(0, TCSANOW, &(msh->term->backup));
 	msh->pid = fork();
@@ -53,9 +71,7 @@ int	exec_command(t_master *msh, char **arg)
 		execve(command, arg, msh->envp);
 	else
 	{
-		waitpid(msh->pid, &return_value, 0);
-		if (WIFEXITED(return_value))
-            msh->return_value = WEXITSTATUS(return_value);
+		parent_wait_pid(msh);
 		free(command);
 	}
 	if (tcsetattr(0, TCSANOW, &msh->term->term) == -1)
